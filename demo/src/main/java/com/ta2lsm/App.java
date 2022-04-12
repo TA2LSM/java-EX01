5ca43bd6b8cbbcaf6fa69357b4de8014ccb8ae97
TA2LSM <semihsenol@gmail.com>
2022-04-12 17:28:41 +0300

    first commit

---
+++ demo/src/main/java/com/ta2lsm/App.java
@@ -0,0 +1,21 @@
+package com.ta2lsm;
+
+/**
+ * JAVA uygulamasına ait ana class
+ */
+public final class App {
+    /**
+     * Uygulamaya özgü dışarıdan erişilemeyecek kodlar
+     */
+    private App() {
+        //...
+    }
+
+    /**
+     * Buraya yazılanlar ilgili metodun açıklamaları olarak gözükecektir.
+     * @param args The arguments of the program.
+     */
+     public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
